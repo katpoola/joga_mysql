d32b528dc73d0e4e6894e8d5d039c6728894a83a
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-02-21 10:35:06 +0200

    Added static css and image files

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.engine('hbs', hbs.engine({
 	layoutsDir: __dirname + '/views/layouts/',
 }))
 
+// setup static public directory
+app.use(express.static('public'));
+
 const mysql = require('mysql')
 
 const bodyParser = require('body-parser')
